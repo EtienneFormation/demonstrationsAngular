81c97451b2a34df7eec7a84f0692f49cc7ec6557
Etienne CASSIN <ecassin@campus-eni.fr>
2024-07-22 15:04:31 +0200

    initial commit

---
+++ src/app/unptinom/unptinom.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-unptinom',
+  standalone: true,
+  imports: [],
+  templateUrl: './unptinom.component.html',
+  styleUrl: './unptinom.component.css'
+})
+export class UnptinomComponent {
+
+}
