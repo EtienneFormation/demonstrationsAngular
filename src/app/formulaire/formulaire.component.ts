2cf749ed109842f2c7f1286fd46e35883cdeefe4
Etienne CASSIN <ecassin@campus-eni.fr>
2024-07-24 16:51:35 +0200

    demo formulaire avec angular

--- src/app/formulaire/formulaire.component.ts
+++ src/app/formulaire/formulaire.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-formulaire',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    NgIf
+  ],
   templateUrl: './formulaire.component.html',
   styleUrl: './formulaire.component.css'
 })
 export class FormulaireComponent {
+  myform : FormGroup;
 
+  constructor() {
+    this.myform = new FormGroup({
+      username : new FormControl('Mathis', Validators.required),
+      age : new FormControl('', [Validators.required, Validators.min(18)]),
+      ddn : new FormControl('', Validators.required),
+      email : new FormControl('', [Validators.required, Validators.email]),
+      password : new FormControl('', [Validators.required, Validators.minLength(9)]),
+      topping1 : new FormControl('', []),
+      topping2 : new FormControl('', []),
+      topping3 : new FormControl('', []),
+      base : new FormControl('', [Validators.required]),
+      sport : new FormControl('', [Validators.required]),
+      grade : new FormControl('', [Validators.required]),
+      commentary : new FormControl('', [])
+    });
+  }
+
+  onFormSubmit() {
+    console.log(this.myform.value);
+    console.log(this.myform.valid);
+  }
 }
